a44fd47c8d0cf12f62005e10ce00219e84aee295
biswajit-dash-00 <biswajit.dash@solytics-partners.com>
2026-05-25 14:09:29 +0530

    init

--- trackit/core/urls.py
+++ trackit/core/urls.py
@@ -1,5 +1,6 @@
 """Core app URL configuration"""
 from django.urls import path
+from django.views.generic import RedirectView
 from core.views import (
     UpdatePageView, AdminDashboardView,
     FilterDetailView, ReportDetailView,
@@ -8,6 +9,7 @@ from core.views import (
 from core.health import health_check, detailed_health_check
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/dashboard/', permanent=False)),
     path('health/', health_check, name='health_check'),
     path('health/detailed/', detailed_health_check, name='detailed_health_check'),
     path('update/<str:token>/', UpdatePageView.as_view(), name='update_page'),
